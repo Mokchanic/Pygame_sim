b987553714e08e0747b20e6c1a37c79a7f0faa1a
MokJinWoo_Player <mokok118@kakao.com>
2021-08-07 19:38:08 +0900

    update_map
    update_map grid 그리는중

--- Make_Map/Map.py
+++ Make_Map/Map.py
@@ -3,6 +3,9 @@ import pygame as pg
 class Map():
     def __init__(self):
         self.done = False
+        self.repeat = 0
+        self.x_length = 0
+        self.y_length = 0
 
     def make_window(self, size): # UI창을 만들어줌.
         self.window = pg.display.set_mode(size)
@@ -19,4 +22,25 @@ class Map():
             if event.type == pg.QUIT: #GUI창에서 X를 누르면
                 self.done = True
         return self.done
+
+    def arena(self, screen, color, init_xy, end_xy, arena_tick):
+        self.draw_arena = pg.draw.rect(screen, color, [init_xy[0], init_xy[1], end_xy[0], end_xy[1]], arena_tick)
+        return self.draw_arena
+
+    def grid(self, screen, color, x_interval, y_interval, thickness, init_arena_xy, end_arena_xy, on_off):
+        self.x_length = end_arena_xy[0] - init_arena_xy[0]
+        self.y_length = end_arena_xy[1] - init_arena_xy[1]
+        self.repeat_and_length = int(self.x_length / x_interval)
+        if on_off == True:
+            for i in range(self.repeat_and_length):
+                init_grid_xy = [init_arena_xy[0] + x_interval, init_arena_xy[1]]
+                end_grid_xy = [init_arena_xy[0] + x_interval, end_arena_xy[1]]
+                self.line_x = pg.draw.line(screen, color, [init_grid_xy[0], init_grid_xy[1]], [end_grid_xy[0], end_grid_xy[1]], thickness)
+
+                # for j in range(self.repeat_and_length):
+                #     init_grid_xy = [init_arena_xy[0], init_arena_xy[1] + self.repeat_and_length]
+                #     end_grid_xy = [end_arena_xy[0], end_arena_xy[1] + self.repeat_and_length]
+                #     self.line_y = pg.draw.line(screen, color, [init_grid_xy[0], init_grid_xy[1]], [end_grid_xy[0], end_grid_xy[1]], thickness)
+        else:
+            pass
 
--- test_map.py
+++ test_map.py
@@ -11,7 +11,13 @@ def main():
     while not done:
         get_map.frame_rate(MAP_FRAME)
         get_map.end_condition()
-        screen.fill(WHITE)
+        screen.fill(WHITE) # 바탕화면을 흰색으로
+
+        get_map.arena(screen, BLACK, INIT_ARENA_XY, END_ARENA_WH, ARENA_THICK)
+        get_map.grid(screen, GRAY, X_INTERVAL, Y_INTERVAL, GRID_THICK, INIT_ARENA_XY, ARENA_END_AXIS, GRID_ON_OFF)
+
+
+        pg.display.flip() # 업데이트
 
 
 
